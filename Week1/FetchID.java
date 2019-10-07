fd6eb714cb7787438d2e2bbed672e6b60dec9871
TylerJClark <56235386+TylerJClark@users.noreply.github.com>
2019-10-07 11:01:31 +0100

    Update FetchID.java

--- Week1/FetchID.java
+++ Week1/FetchID.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-public class FetchID {//sp5r11
+public class FetchID {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));	
